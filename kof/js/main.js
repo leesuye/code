9e76025f5c78201d785691cbae2a61c58d204f4e
leesuye <freely0320@naver.com>
2022-10-04 17:42:11 +0900

    에러

--- kof/js/main.js
+++ kof/js/main.js
@@ -14,4 +14,9 @@ $(document).ready(function(){
         $('.notice ul li').removeClass('active');
         $(this).addClass('active');
    });
+
+    /* 스크롤 애니메이션 라이브러리 */
+    AOS.init();
+
+
 });//document.ready 종료
