f1c9f98802ce4bc6c554b6397750ac61bec37f0c
leesuye <freely0320@naver.com>
2022-10-19 18:00:31 +0900

    111

--- children/js/main.js
+++ children/js/main.js
@@ -4,16 +4,31 @@ $(document).ready(function(){
 
     const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-    navigation: true, /* 오른쪽에 각 페이지의 paging */
-    navigationPosition: 'right', /* 위치 */
-    navigationTooltips: ['메인', '보건통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
-    showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+        navigation: true, /* 오른쪽에 각 페이지의 paging */
+        navigationPosition: 'right', /* 위치 */
+        navigationTooltips: ['메인', '보건통계', '사업소개', '현장소식', '지원사업'], /* 툴팁 */
+        showActiveTooltip: false, /* 현재 활성화된 페이지의 툴팁 보이기 */
 
-    /*autoScrolling:true, 한페이지씩 스크롤
-    scrollHorizontally: true, */
+        /*autoScrolling:true, 한페이지씩 스크롤
+        scrollHorizontally: true, */
 
-    /* verticalCentered: true, 컨텐츠 요소 위아래 가운데 */
-    responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+        afterLoad: function(origin, destination, direction, trigger){
+        //    console.log(destination.index);
+            if((destination.index == 2)||(destination.index == 3)||(destination.index == 4)||(destination.index == 5)){
+                $('.header').addClass('black');
+            }else{
+                $('.header').removeClass('black');
+            }
+        },
+
+        /* verticalCentered: true, 컨텐츠 요소 위아래 가운데 */
+        responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
+	});//fullpage
+
+    //count 숫자 count-up
+    // $('.count .rate ul li .num strong').counterUp({   /* 상세설정을 해주는 경우 */
+    //     delay: 10,   /* 애니메이션이 작동되기 이전 대기 시간 */
+    //     time: 2000    /* 전체 애니메이션 시간 */
+    // });//countup
     
-	});
 });//document.ready
