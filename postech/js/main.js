d98f635b1bfd5490beb0c6873c2101d8803e968f
leesuye <freely0320@naver.com>
2022-11-07 18:03:33 +0900

    포항공대

--- postech/js/main.js
+++ postech/js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
-   
-})
+    const swiper = new Swiper('.visual .popup', {
 
+        effect: "fade", 
+
+        autoplay: {  
+            delay: 4000,
+            disableOnInteraction: true,
+        },
+
+        loop: true,  
+
+        pagination: { 
+            el: '.btn_paging',
+            clickable: true,
+        },
+
+    });//visual swiper
+
+});//document
 
