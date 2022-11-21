4b6f22875cc03cfaf1138169f0bf295f06e36ac4
leesuye <freely0320@naver.com>
2022-11-21 18:01:27 +0900

    스와

---
+++ swarovski/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    let idx = 1
+    
+    setInterval (function(){
+        if( idx < 3){
+            idx ++
+        }else{
+            idx = 1
+        }
+
+        $('.visual ul li').removeClass('active')
+        $('.visual ul li').eq(idx-1).addClass('active')
+    },4000);
+    
+
+
+});
+
+
+
+
+
+
